a5e34f4bde6eec0c7dbff5c34608687527419c18
Tom Plüss <tom.pluess@skyr.ch>
2012-12-09 12:36:57 +0100

    first commit from eclipse project.

---
+++ howlong/src/main/java/ch/skyr/howlong/client/PhoneActivityMapper.java
@@ -0,0 +1,25 @@
+
+package ch.skyr.howlong.client;
+
+import com.google.gwt.activity.shared.Activity;
+import com.google.gwt.activity.shared.ActivityMapper;
+import com.google.gwt.place.shared.Place;
+
+
+/**
+ * 
+ */
+public class PhoneActivityMapper implements ActivityMapper {
+
+	private final ClientFactory clientFactory;
+
+	public PhoneActivityMapper(ClientFactory clientFactory) {
+		this.clientFactory = clientFactory;
+	}
+
+	@Override
+	public Activity getActivity(Place place) {
+		//TODO place your activities here
+		return null;
+	}
+}
